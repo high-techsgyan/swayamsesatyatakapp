92829ba233d3dec3634ff65ba32acb56b3cdfecd
lakshman898650 <lakshman898650@gmail.com>
2024-11-04 11:47:23 +0000

    doing some changes

--- android/app/src/main/kotlin/com/hightechsgyan/swayamsesatyatak/MainActivity.kt
+++ android/app/src/main/kotlin/com/hightechsgyan/swayamsesatyatak/MainActivity.kt
@@ -1,5 +1,49 @@
 package com.swayamsesatyatak.achintya
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import androidx.annotation.NonNull
+import androidx.core.content.FileProvider
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import java.io.File
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.swayamsesatyatak.achintya/install"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "installApk") {
+                val filePath = call.argument<String>("filePath")
+                if (filePath != null) {
+                    installApk(filePath)
+                    result.success(null)
+                } else {
+                    result.error("INVALID_ARGUMENT", "File path is null", null)
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+   private fun installApk(filePath: String) {
+    val apkFile = File(filePath)
+    if (apkFile.exists()) {
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            setDataAndType(
+                FileProvider.getUriForFile(this@MainActivity, "${packageName}.fileprovider", apkFile),
+                "application/vnd.android.package-archive"
+            )
+            addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+        startActivity(intent)
+    } else {
+        throw IllegalArgumentException("APK file does not exist: $filePath")
+    }
+}
+}
--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,15 +1,9 @@
-import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:go_router/go_router.dart';
-import 'package:open_file/open_file.dart';
-import 'package:path_provider/path_provider.dart';
-import 'dart:io';
-import 'dart:convert';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
+  const SplashScreen({Key? key}) : super(key: key);
 
   @override
   _SplashScreenState createState() => _SplashScreenState();
@@ -17,134 +11,12 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  final String _supabaseUrl = "https://gksxshcgikuirnfhbxbw.supabase.co";
-  final String _apiKey =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdrc3hzaGNnaWt1aXJuZmhieGJ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA2ODYzMTAsImV4cCI6MjA0NjI2MjMxMH0.5xuyPQsCgxfMmzbQFofMvcWG-TE7P3lwuIYoa8_ZOm0"; // Your API key
-  final String currentVersion = "1.0.1"; // Your current app version
 
   @override
   void initState() {
     super.initState();
-    _checkForUpdates();
-  }
-
-  Future<void> _checkForUpdates() async {
-    try {
-      final response = await http.get(
-        Uri.parse(
-            '$_supabaseUrl/rest/v1/app_versions?select=*&order=created_at.desc&limit=1'),
-        headers: {
-          'Authorization': 'Bearer $_apiKey',
-          'apikey': _apiKey,
-          'Content-Type': 'application/json'
-        },
-      );
-
-      print('Response status: ${response.statusCode}');
-      print('Response body: ${response.body}');
-
-      if (response.statusCode == 200) {
-        final List<dynamic> versions = json.decode(response.body);
-
-        // Ensure there is at least one version available
-        if (versions.isNotEmpty) {
-          final String latestVersion = versions[0]['version'];
-          final String downloadUrl = versions[0]['url'];
-
-          // Compare with the current app version
-          if (latestVersion != currentVersion) {
-            _showUpdateDialog(latestVersion, downloadUrl);
-          } else {
-            _navigateToNextScreen();
-          }
-        } else {
-          _showErrorDialog("No version information available.");
-        }
-      } else {
-        _showErrorDialog(
-            "Failed to fetch version info. Status code: ${response.statusCode}");
-      }
-    } catch (e) {
-      print("Network error: $e");
-      _showErrorDialog("Network error: ${e.toString()}");
-    }
-  }
-
-  void _showUpdateDialog(String latestVersion, String downloadUrl) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Update Available'),
-          content: Text(
-              'A new version $latestVersion is available. Do you want to update?'),
-          actions: [
-            TextButton(
-              child: Text('Ignore'),
-              onPressed: () {
-                Navigator.of(context).pop();
-                _navigateToNextScreen();
-              },
-            ),
-            TextButton(
-              child: Text('Update'),
-              onPressed: () async {
-                Navigator.of(context).pop();
-                await _downloadAndUpdate(downloadUrl);
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  Future<void> _downloadAndUpdate(String downloadUrl) async {
-    try {
-      final response = await http.get(Uri.parse(downloadUrl));
-      if (response.statusCode == 200) {
-        final Directory tempDir = await getTemporaryDirectory();
-        final File file = File('${tempDir.path}/app-release.apk');
-        await file.writeAsBytes(response.bodyBytes);
-
-        // Show download progress (optional)
-        _showDownloadProgress(file.path);
-      } else {
-        _showErrorDialog(
-            "Failed to download the update. Status code: ${response.statusCode}");
-      }
-    } catch (e) {
-      print("Download error: $e");
-      _showErrorDialog("Download error: ${e.toString()}");
-    }
-  }
-
-  void _showDownloadProgress(String filePath) {
-    // Show a dialog or a snackbar with progress information
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Download Progress'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text('Downloading APK...'),
-              SizedBox(height: 20),
-              CircularProgressIndicator(),
-            ],
-          ),
-        );
-      },
-    );
-
-    // Simulating download completion and opening the APK file
-    Future.delayed(Duration(seconds: 2), () {
-      OpenFile.open(filePath);
-      Navigator.of(context).pop(); // Close the dialog after opening the file
-      Timer(Duration(seconds: 5),
-          _navigateToNextScreen); // Navigate to next screen after a delay
-    });
+    // Start the navigation process after a delay
+    Future.delayed(const Duration(seconds: 2), _navigateToNextScreen);
   }
 
   void _navigateToNextScreen() {
@@ -161,11 +33,11 @@ class _SplashScreenState extends State<SplashScreen> {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('Error'),
+          title: const Text('Error'),
           content: Text(message),
           actions: [
             TextButton(
-              child: Text('OK'),
+              child: const Text('OK'),
               onPressed: () {
                 Navigator.of(context).pop();
                 _navigateToNextScreen();
@@ -185,8 +57,8 @@ class _SplashScreenState extends State<SplashScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Image.asset('assets/images/swayam_se_satya_tak.png', width: 200),
-            SizedBox(height: 20),
-            CircularProgressIndicator(),
+            const SizedBox(height: 20),
+            const CircularProgressIndicator(),
           ],
         ),
       ),
